377be67d83b77d75f5417f5966b67896d7a1fd4a
Ghazanfar Ali Anwar <72309489+ghazanfaran@users.noreply.github.com>
2021-05-12 11:10:48 +0800

    Add files via upload

---
+++ 8-Story models/2-FRP/8_TH/CFRP/Run.bat
@@ -0,0 +1,22 @@
+reader 8TH3 8STH3.zzz
+solver 8TH3 8TH3.zzz
+PostProc 8TH3.num Monitor-Defor.mon Monitor3.txt
+del *.pat
+del *.plt
+del *.bak
+del *.mpf
+del *.cnd
+del *.rnk
+del *.eig
+del *.int
+del *.lod
+del *.per
+del *.phs
+del *.ref
+del *.rpr
+del *.sbd
+del *.spr
+del *.stg
+del *.mat
+del *.tmp
+del *.zzz del *.num
---
+++ Reader-Sover-PostProcessor/Run-TH-Hospital-YDir-016.bat
@@ -0,0 +1,54 @@
+reader Hospital-TH-008 Hospital-TH-008.zzz
+solver Hospital-TH-008 Hospital-TH-008.zzz
+PostProc Hospital-TH-008.num Monitor-Hospital-Y-D-Defor.mon Monitor-Hospital-Y-D-Defor-008.txt
+PostProc Hospital-TH-008.num Monitor-Hospital-Y-D-SC-ALL.mon Monitor-Hospital-Y-D-SC-ALL-008.txt
+PostProc Hospital-TH-008.num Monitor-Hospital-Y-D-SS-ALL.mon Monitor-Hospital-Y-D-SS-ALL-008.txt
+PostProc Hospital-TH-008.num Monitor-Hospital-Y-D-F-ALL.mon Monitor-Hospital-Y-D-F-ALL-008.txt
+PostProc Hospital-TH-008.num Monitor-Hospital-Y-D-My1-ALL.mon Monitor-Hospital-Y-D-My1-ALL-008.txt
+PostProc Hospital-TH-008.num Monitor-Hospital-Y-D-My2-ALL.mon Monitor-Hospital-Y-D-My2-ALL-008.txt
+del *.pat
+del *.plt
+del *.bak
+del *.mpf
+del *.cnd
+del *.rnk
+del *.eig
+del *.int
+del *.lod
+del *.per
+del *.phs
+del *.ref
+del *.rpr
+del *.sbd
+del *.spr
+del *.stg
+del *.mat
+del *.tmp
+del *.zzz del *.num
+reader Hospital-TH-016 Hospital-TH-016.zzz
+solver Hospital-TH-016 Hospital-TH-016.zzz
+PostProc Hospital-TH-016.num Monitor-Hospital-Y-D-Defor.mon Monitor-Hospital-Y-D-Defor-016.txt
+PostProc Hospital-TH-016.num Monitor-Hospital-Y-D-SC-ALL.mon Monitor-Hospital-Y-D-SC-ALL-016.txt
+PostProc Hospital-TH-016.num Monitor-Hospital-Y-D-SS-ALL.mon Monitor-Hospital-Y-D-SS-ALL-016.txt
+PostProc Hospital-TH-016.num Monitor-Hospital-Y-D-F-ALL.mon Monitor-Hospital-Y-D-F-ALL-016.txt
+PostProc Hospital-TH-016.num Monitor-Hospital-Y-D-My1-ALL.mon Monitor-Hospital-Y-D-My1-ALL-016.txt
+PostProc Hospital-TH-016.num Monitor-Hospital-Y-D-My2-ALL.mon Monitor-Hospital-Y-D-My2-ALL-016.txt
+del *.pat
+del *.plt
+del *.bak
+del *.mpf
+del *.cnd
+del *.rnk
+del *.eig
+del *.int
+del *.lod
+del *.per
+del *.phs
+del *.ref
+del *.rpr
+del *.sbd
+del *.spr
+del *.stg
+del *.mat
+del *.tmp
+del *.zzz del *.num
