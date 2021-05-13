7ffeeb36d63c5f6fcd4bfb10513819a40a8042b0
Ghazanfar Ali Anwar <72309489+ghazanfaran@users.noreply.github.com>
2021-05-13 14:54:37 +0800

    2 story models in Zues-NL

---
+++ 2-Story Models/2-FRP/2_TH_CFRP_All/Run_0.64.bat
@@ -0,0 +1,160 @@
+reader 2-story-TH0 2-story-TH0.zzz
+solver 2-story-TH0 2-story-TH0.zzz
+PostProc 2-story-TH0.num Monitor-Defor.mon Monitor0.txt
+del *.pat
+del *.plt
+del *.bak
+del *.mpf
+del *.cnd
+del *.rnk
+del *.eig
+del *.int
+del *.lod
+del *.per
+del *.phs
+del *.ref
+del *.rpr
+del *.sbd
+del *.spr
+del *.stg
+del *.mat
+del *.tmp
+del *.zzz del *.num
+
+reader 2-story-TH1 2-story-TH1.zzz
+solver 2-story-TH1 2-story-TH1.zzz
+PostProc 2-story-TH1.num Monitor-Defor.mon Monitor1.txt
+del *.pat
+del *.plt
+del *.bak
+del *.mpf
+del *.cnd
+del *.rnk
+del *.eig
+del *.int
+del *.lod
+del *.per
+del *.phs
+del *.ref
+del *.rpr
+del *.sbd
+del *.spr
+del *.stg
+del *.mat
+del *.tmp
+del *.zzz del *.num
+
+reader 2-story-TH2 2-story-TH2.zzz
+solver 2-story-TH2 2-story-TH2.zzz
+PostProc 2-story-TH2.num Monitor-Defor.mon Monitor2.txt
+del *.pat
+del *.plt
+del *.bak
+del *.mpf
+del *.cnd
+del *.rnk
+del *.eig
+del *.int
+del *.lod
+del *.per
+del *.phs
+del *.ref
+del *.rpr
+del *.sbd
+del *.spr
+del *.stg
+del *.mat
+del *.tmp
+del *.zzz del *.num
+
+reader 2-story-TH3 2-story-TH3.zzz
+solver 2-story-TH3 2-story-TH3.zzz
+PostProc 2-story-TH3.num Monitor-Defor.mon Monitor3.txt
+del *.pat
+del *.plt
+del *.bak
+del *.mpf
+del *.cnd
+del *.rnk
+del *.eig
+del *.int
+del *.lod
+del *.per
+del *.phs
+del *.ref
+del *.rpr
+del *.sbd
+del *.spr
+del *.stg
+del *.mat
+del *.tmp
+del *.zzz del *.num
+
+reader 2-story-TH4 2-story-TH4.zzz
+solver 2-story-TH4 2-story-TH4.zzz
+PostProc 2-story-TH4.num Monitor-Defor.mon Monitor4.txt
+del *.pat
+del *.plt
+del *.bak
+del *.mpf
+del *.cnd
+del *.rnk
+del *.eig
+del *.int
+del *.lod
+del *.per
+del *.phs
+del *.ref
+del *.rpr
+del *.sbd
+del *.spr
+del *.stg
+del *.mat
+del *.tmp
+del *.zzz del *.num
+
+reader 2-story-TH5 2-story-TH5.zzz
+solver 2-story-TH5 2-story-TH5.zzz
+PostProc 2-story-TH5.num Monitor-Defor.mon Monitor5.txt
+del *.pat
+del *.plt
+del *.bak
+del *.mpf
+del *.cnd
+del *.rnk
+del *.eig
+del *.int
+del *.lod
+del *.per
+del *.phs
+del *.ref
+del *.rpr
+del *.sbd
+del *.spr
+del *.stg
+del *.mat
+del *.tmp
+del *.zzz del *.num
+
+reader 2-story-TH6 2-story-TH6.zzz
+solver 2-story-TH6 2-story-TH6.zzz
+PostProc 2-story-TH6.num Monitor-Defor.mon Monitor6.txt
+del *.pat
+del *.plt
+del *.bak
+del *.mpf
+del *.cnd
+del *.rnk
+del *.eig
+del *.int
+del *.lod
+del *.per
+del *.phs
+del *.ref
+del *.rpr
+del *.sbd
+del *.spr
+del *.stg
+del *.mat
+del *.tmp
+del *.zzz del *.num
